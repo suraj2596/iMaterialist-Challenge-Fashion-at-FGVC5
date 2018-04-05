939bab9c41fe8fd5c6355e940072e539893163eb
suraj <suraj2596@gmail.com>
2018-04-05 12:08:41 +0530

    compressed datasets

---
+++ download_im.py
@@ -0,0 +1,8 @@
+import json
+data = json.load(open('validation.json'))
+
+#for i in range(len(data['images'])):
+img_url = data['images'][0]['url']
+img_name = data['images'][0]['imageId'] 
+print img_url
+print img_name
